5467393b29261771858eba7019eaf599f9c5c5a2
edgarrosa2 <rosado12ea@gmail.com>
2026-04-13 22:32:52 -0400

    Create ranking.module.ts

---
+++ src/app/pages/Ranking/ranking.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
+import { RankingPage } from './ranking.page';
+
+const routes: Routes = [
+  { path: '', component: RankingPage }
+];
+
+@NgModule({
+  declarations: [RankingPage],
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class RankingPageModule {}
